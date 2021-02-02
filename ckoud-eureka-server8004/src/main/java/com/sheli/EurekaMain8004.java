dda5358755d9f5cda96ee81d48cba49b442d0b39
lishen <amonra@126.com>
2021-02-02 21:21:18 +0800

    Add multi Eureka registers

---
+++ ckoud-eureka-server8004/src/main/java/com/sheli/EurekaMain8004.java
@@ -0,0 +1,13 @@
+package com.sheli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain8004.class);
+    }
+}
